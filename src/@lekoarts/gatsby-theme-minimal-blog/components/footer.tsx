aa1cd5e9ba9a59425ea2eb7040e48f2f63b2cff0
Bashiru <bashirushardow@gmail.com>
2020-12-22 00:42:47 +0000

    hi

--- src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -1,13 +1,14 @@
 <footer class="css-6x8z6y">
-    <div>© 
+    <div>
+        "©" 
         <!-- -->
-        2020
+        "2020"
         <!-- --> 
-        by 
+        "by"
         <!-- -->
-        Bashiru Shardow
+        "Bashiru Shardow"
         <!-- -->
-        . All rights reserved.
+        ". All rights reserved."
     </div>
     <div>
         <a aria-label="Link to the theme's GitHub repository" href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-minimal-blog" class="css-688a3f">Theme</a> <!-- -->by<!-- --> <a aria-label="Link to the theme author's website" href="https://www.lekoarts.de/en" class="css-688a3f">LekoArts</a>
