7ee03684d7365225488a339f7158d046a4b88876
Bashiru <bashirushardow@gmail.com>
2020-12-22 00:38:23 +0000

    wow

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/header.tsx
@@ -0,0 +1,18 @@
+<header class="css-1wt4r56">
+    <div class="css-190uhut">
+        <a aria-current="page" aria-label="Bashiru Shardow - Back to home" class="css-dz3ojy" href="/">
+            <div class="css-1kmktz0">Bashiru Shardow</div>
+        </a>
+        <button type="button" aria-label="Activate Dark Mode" title="Activate Dark Mode" class="css-af3ja4"><div class="css-iiu7h7"></div></button>
+        </div>
+        <div class="css-1kvspmd">
+            <nav class="css-nrb238">
+                <a class="css-688a3f" href="/blog">Blog</a>
+                <a class="css-688a3f" href="/about">About</a>
+            </nav>
+        <div class="css-e5k4e3">
+            <a href="https://twitter.com/bssmoove" class="css-688a3f">Twitter</a>
+            <a href="https://www.instagram.com/basssshiru/" class="css-688a3f">Instagram</a>
+        </div>
+    </div>
+</header>
