46b031aeaf70833e7cbdaf1e46356c4c3fe00dd8
Bashiru <bashirushardow@gmail.com>
2020-12-22 00:58:07 +0000

    yoh

--- src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -7,6 +7,6 @@
         ". All rights reserved."
     </div>
     <div>
-        <a aria-label="Link to the theme's GitHub repository" href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-minimal-blog" class="css-688a3f">Theme</a> <!-- -->by<!-- --> <a aria-label="Link to the theme author's website" href="https://www.lekoarts.de/en" class="css-688a3f">LekoArts</a>
+        <a aria-label="Link to the theme's GitHub repository" href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-minimal-blog" class="css-688a3f">Theme</a> by <a aria-label="Link to the theme author's website" href="https://www.lekoarts.de/en" class="css-688a3f">LekoArts</a>
     </div>
 </footer>
